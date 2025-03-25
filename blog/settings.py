44c02fab912d37feabe3c9381074939a930498a6
no-0-name <87208495+no-0-name@users.noreply.github.com>
2025-03-25 23:01:26 +0300

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-_bdbd2xxbmgp6io)2chh5n)$^ji%dpr@zsg2fzcz1#4urf9!-k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
